be080196fb000cd7e906445eff6e8a01dadca57a
Renzo Navarro <rcnavarrop@gmail.com>
2022-07-02 14:56:36 -0400

    Creates ViewCart button

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Divider } from "react-native-elements/dist/divider/Divider";
+import SafeArea from "../components/home/SafeArea";
 import About from "../components/restaurantDetail/About";
 import MenuItems from "../components/restaurantDetail/MenuItems";
+import ViewCart from "../components/restaurantDetail/ViewCart";
 
-export default function RestaurantDetail({route}) {
+export default function RestaurantDetail({ route, navigation }) {
   return (
-    <View>
+    <SafeArea>
       <About route={route} />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
       <MenuItems />
-    </View>
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
+    </SafeArea>
   );
 }
